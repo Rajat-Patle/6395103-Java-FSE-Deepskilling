19cc36e0ea1f57ef42e6e921242295d620fed126
Rajat Nareshsing Patle <118557334+Rajat-Patle@users.noreply.github.com>
2025-06-19 06:42:10 +0530

    Delete FactoryMethodPatternExample/src directory

--- FactoryMethodPatternExample/src/documents/PDFDocument.java
+++
@@ -1,7 +0,0 @@
-package documents;
-
-public class PDFDocument implements Document {
-    public void open() {
-        System.out.println("Opening PDF Document");
-    }
-}
--- FactoryMethodPatternExample/src/factoryClass/DocumentFactory.java
+++
@@ -1,7 +0,0 @@
-package factoryClass;
-
-import documents.Document;
-
-public abstract class DocumentFactory {
-    public abstract Document createDocument();
-}
