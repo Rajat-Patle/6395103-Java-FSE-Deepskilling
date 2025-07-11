ed525bc0dc9537b35c1f648f8fd0c5587120f383
Rajat Nareshsing Patle <118557334+Rajat-Patle@users.noreply.github.com>
2025-07-11 11:53:35 +0530

    Add files via upload

---
+++ Week-4/country-code-service/src/test/java/com/cognizant/springlearn/country_code_service/CountryCodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn.country_code_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryCodeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
