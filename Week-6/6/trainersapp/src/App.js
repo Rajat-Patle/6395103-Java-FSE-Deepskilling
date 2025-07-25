d55a85c9d05436c4bc6cf314663700c6f35fc973
Rajat-Patle <rajatnareshsingpatle@gmail.com>
2025-07-25 22:54:57 +0530

    React Handson 6

---
+++ Week-6/6/trainersapp/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import TrainerList from './TrainerList';
+import TrainerDetails from './TrainerDetails';
+import trainers from './TrainersMock';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Trainer Management Portal</h1>
+        <nav>
+          <Link to="/" style={{ marginRight: '20px' }}>Home</Link>
+          <Link to="/trainers">Trainers</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/trainers" element={<TrainerList trainers={trainers} />} />
+          <Route path="/trainer/:id" element={<TrainerDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Week-6/6/trainersapp/src/TrainersMock.js
@@ -0,0 +1,9 @@
+import Trainer from './trainer';
+
+const trainers = [
+  new Trainer(1, "Aathma", "aathma@mail.com", "9876543210", "Java", ["Spring", "Hibernate"]),
+  new Trainer(2, "Apoorv", "apoorv@mail.com", "9876501234", "React", ["ReactJS", "Redux"]),
+  new Trainer(3, "Elisa", "elisa@mail.com", "9876567890", "Python", ["Django", "Flask"]),
+];
+
+export default trainers;
